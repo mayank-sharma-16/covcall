a9245f61a2b923b511d9772b3e548a1ea93f7e11
Shreyas Krishnaswamy <shrekris@berkeley.edu>
2020-03-29 05:49:05 -0700

    Implement outbound calls.

--- callmanager.py
+++ callmanager.py
@@ -1,8 +1,8 @@
 # Download the Python helper library from twilio.com/docs/python/install
 from twilio.rest import Client
+from twilio.twiml.voice_response import VoiceResponse
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 import os
-
-from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -11,11 +11,25 @@ client = Client(account_sid, auth_token)
 
 # Make API calls here...
 
-message = client.messages \
-    .create(
-         body="Even lemons can at least power a clock",
-         messaging_service_sid='MG6a8cbff2990e52e3b5699115738f7b91',
-         to='+19256405980'
-     )
+def send_message(number, text):
+    message = client.messages \
+        .create(
+             body=text,
+             messaging_service_sid='MG6a8cbff2990e52e3b5699115738f7b91',
+             to=number
+         )
 
-print(message.sid)
+def generate_call_message(text):
+    f = open("static/covcall.xml", "wb")
+    top = Element('Response')
+    child = SubElement(top, 'Say', {'voice': 'alice'})
+    child.text = text
+    print(tostring(top))
+    f.write(tostring(top))
+
+def make_call(number):
+    call = client.calls.create(
+                        url='http://0c962c1f.ngrok.io/static/covcall.xml',
+                        to=number,
+                        from_='+14073293385'
+                    )
--- callserver.py
+++ callserver.py
@@ -1,54 +1,62 @@
 from flask import Flask, request, send_file
 from twilio.twiml.voice_response import VoiceResponse, Gather
+from callmanager import send_message
 
 app = Flask(__name__)
 
 
-@app.route("/voice", methods=['POST'])
-def voice():
-    """Respond to incoming phone calls and mention the caller's city"""
-    intro = False
-    number = request.values['From'] # Get caller's number
-    state = request.values['FromState'] # Get caller's state
+@app.route("/<path:text>", methods=['POST'])
+def voice(text):
+    if "static" in text:
+        return send_file(text, mimetype='text/xml', as_attachment=True)
+    else:
+        """Respond to incoming phone calls and mention the caller's city"""
+        intro = False
+        number = request.values['From'] # Get caller's number
+        state = request.values['FromState'] # Get caller's state
 
-    # Start our TwiML response
-    resp = VoiceResponse()
+        # Start our TwiML response
+        resp = VoiceResponse()
 
-    if 'Digits' in request.values:
-        # Get which digit the caller chose
-        choice = request.values['Digits']
+        if 'Digits' in request.values:
+            # Get which digit the caller chose
+            choice = request.values['Digits']
 
-        # <Say> a different message depending on the caller's choice
-        if choice == '1':
-            resp.play('speeches/joinspeech.mp3')
-            addNumber(number, state)
-            return str(resp)
-        elif choice == '2':
-            resp.play('speeches/leavespeech.mp3')
-            removeNumber(number, state)
-            return str(resp)
-        else:
-            # If the caller didn't choose 1 or 2, apologize and ask them again
-            intro = True
-            resp.play('speeches/redospeech.mp3')
+            # <Say> a different message depending on the caller's choice
+            if choice == '1':
+                resp.play('static/joinspeech.mp3')
+                addNumber(number, state)
+                return str(resp)
+            elif choice == '2':
+                resp.play('static/leavespeech.mp3')
+                removeNumber(number, state)
+                return str(resp)
+            elif choice == '3':
+                resp.play('static/moreinfospeech.mp3')
+                send_message(number, "For more information, please visit our website at COVcall.tech")
+                return str(resp)
+            else:
+                # If the caller didn't choose 1 or 2, apologize and ask them again
+                intro = True
+                resp.play('static/redospeech.mp3')
 
-    # Read intro message aloud to the caller
-    if not intro:
-        resp.play('speeches/introspeech.mp3')
+        # Read intro message aloud to the caller
+        if not intro:
+            resp.play('static/introspeech.mp3')
 
-    # Start our <Gather> verb
-    gather = Gather(num_digits=1)
-    gather.play('speeches/gatherspeech.mp3')
-    resp.append(gather)
+        # Start our <Gather> verb
+        gather = Gather(num_digits=1)
+        gather.play('static/newgatherspeech.mp3')
+        resp.append(gather)
 
-    # If the user doesn't select an option, redirect them into a loop
-    resp.redirect('/voice')
+        # If the user doesn't select an option, redirect them into a loop
+        resp.redirect('/voice')
 
-    return str(resp) # Dummy return statement
+        return str(resp) # Dummy return statement
 
-@app.route("/speeches/<path:text>", methods=['GET'])
+@app.route("/static/<path:text>", methods=['GET'])
 def serve_redospeech(text):
-    return send_file("speeches/" + text, mimetype='audio/mpeg', as_attachment=True)
+        return send_file("static/" + text, mimetype='audio/mpeg', as_attachment=True)
 
 def addNumber(number, state):
     return
